7f2814f5077c2f2b383d1c9f3cc707e3d4149e3e
Jing Guan <jingg.cxy@foxmail.com>
2017-05-12 16:59:43 +0800

    Angular Scope
    - Understand and use scopes to appropriately make your data available
    to your views through the controller.
    - Understand nesting of scopes

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # conFusion
 A full stack web project for `conFusion Restuarant`.
 
-# Coursera
+# Refer
 [Full Stack Web Development](https://www.coursera.org/specializations/full-stack)
+
+# Deploy
+
+```
+npm install -g bower
+bower install
+
+npm install -g gulp
+npm install gulp --save-dev
+npm install jshint gulp-jshint jshint-stylish gulp-imagemin gulp-concat gulp-uglify gulp-minify-css gulp-usemin gulp-cache gulp-changed gulp-rev gulp-rename gulp-notify browser-sync del gulp-ng-annotate --save-dev
+gulp
+gulp watch
+```
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,70 +1,78 @@
 'use strict';
-angular.module('confusionApp', []).controller('menuController', function() {
-    this.tab = 1;
-    this.filtText = '';
 
-    var dishes = [
-        {
-            name:'Uthapizza',
-            image: 'images/uthapizza.png',
-            category: 'mains',
-            label:'Hot',
-            price:'4.99',
-            description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-            comment: ''
-        },
+angular.module('confusionApp', [])
+    .controller('MenuController', ['$scope', function ($scope) {
 
-        {
-            name:'Zucchipakoda',
-            image: 'images/zucchipakoda.png',
-            category: 'appetizer',
-            label:'',
-            price:'1.99',
-            description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
-            comment: ''
-        },
+        $scope.tab = 1;
+        $scope.filtText = '';
+        $scope.showDetails = false;
 
-        {
-            name:'Vadonut',
-            image: 'images/vadonut.png',
-            category: 'appetizer',
-            label:'New',
-            price:'1.99',
-            description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
-            comment: ''
-        },
+        var dishes = [
+            {
+                name: 'Uthapizza',
+                image: 'images/uthapizza.png',
+                category: 'mains',
+                label: 'Hot',
+                price: '4.99',
+                description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+                comment: ''
+            },
 
-        {
-            name:'ElaiCheese Cake',
-            image: 'images/elaicheesecake.png',
-            category: 'dessert',
-            label:'',
-            price:'2.99',
-            description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
-            comment: ''
-        }
-    ];
+            {
+                name: 'Zucchipakoda',
+                image: 'images/zucchipakoda.png',
+                category: 'appetizer',
+                label: '',
+                price: '1.99',
+                description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+                comment: ''
+            },
 
-    this.dishes = dishes;
+            {
+                name: 'Vadonut',
+                image: 'images/vadonut.png',
+                category: 'appetizer',
+                label: 'New',
+                price: '1.99',
+                description: 'A quintessential ConFusion experience, is it a vada or is it a donut?',
+                comment: ''
+            },
 
-    this.select = function(setTab) {
-        this.tab = setTab;
+            {
+                name: 'ElaiCheese Cake',
+                image: 'images/elaicheesecake.png',
+                category: 'dessert',
+                label: '',
+                price: '2.99',
+                description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+                comment: ''
+            }
+        ];
 
-        if (setTab === 2) {
-            this.filtText = "appetizer";
-        }
-        else if (setTab === 3) {
-            this.filtText = "mains";
-        }
-        else if (setTab === 4) {
-            this.filtText = "dessert";
-        }
-        else {
-            this.filtText = "";
-        }
-    };
+        $scope.dishes = dishes;
 
-    this.isSelected = function (checkTab) {
-        return (this.tab === checkTab);
-    };
-});
+        $scope.select = function (setTab) {
+            $scope.tab = setTab;
+
+            if (setTab === 2) {
+                $scope.filtText = "appetizer";
+            }
+            else if (setTab === 3) {
+                $scope.filtText = "mains";
+            }
+            else if (setTab === 4) {
+                $scope.filtText = "dessert";
+            }
+            else {
+                $scope.filtText = "";
+            }
+        };
+
+        $scope.isSelected = function (checkTab) {
+            return ($scope.tab === checkTab);
+        };
+
+        $scope.toggleDetails = function () {
+            $scope.showDetails = !$scope.showDetails;
+        };
+    }]);
