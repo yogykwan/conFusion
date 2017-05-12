e5e463323e1be3c3a7d09edcbc66cbadb0be8d38
Jing Guan <jingg.cxy@foxmail.com>
2017-05-12 17:40:48 +0800

    Angular Forms and Form Validation
    - Set up a form to use Angular support including two-way data binding.
    - Use Angular's support for forms and form validation.
    - Use JavaScript support for form validation.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -75,4 +75,35 @@ angular.module('confusionApp', [])
         $scope.toggleDetails = function () {
             $scope.showDetails = !$scope.showDetails;
         };
+    }])
+
+    .controller('ContactController', ['$scope', function ($scope) {
+
+        $scope.feedback = {
+            mychannel: "", firstName: "", lastName: "", agree: false, email: ""
+        };
+        var channels = [{value: "tel", label: "Tel."}, {value: "Email", label: "Email"}];
+
+        $scope.channels = channels;
+        $scope.invalidChannelSelection = false;
+
+    }])
+
+    .controller('FeedbackController', ['$scope', function ($scope) {
+        $scope.sendFeedback = function () {
+
+            console.log($scope.feedback);
+
+            if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+                $scope.invalidChannelSelection = true;
+                console.log('incorrect');
+            }
+            else {
+                $scope.invalidChannelSelection = false;
+                $scope.feedback = {mychannel: "", firstName: "", lastName: "", agree: false, email: ""};
+                $scope.feedback.mychannel = "";
+                $scope.feedbackForm.$setPristine();
+                console.log($scope.feedback);
+            }
+        };
     }]);
