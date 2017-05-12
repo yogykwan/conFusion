0f49034a0dfe748cf4d44bbea12d9362c8481e13
Jing Guan <jingg.cxy@foxmail.com>
2017-05-12 21:34:02 +0800

    JS2: Task Runners, Angular Scope, Forms and Form Validation
    - Add dish comment form
    - Validate form elements

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -106,4 +106,75 @@ angular.module('confusionApp', [])
                 console.log($scope.feedback);
             }
         };
-    }]);
+    }])
+
+    .controller('DishDetailController', ['$scope', function ($scope) {
+
+        $scope.orderText = '';
+
+        var dish = {
+            name: 'Uthapizza',
+            image: 'images/uthapizza.png',
+            category: 'mains',
+            label: 'Hot',
+            price: '4.99',
+            description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+            comments: [
+                {
+                    rating: 5,
+                    comment: "Imagine all the eatables, living in conFusion!",
+                    author: "John Lemon",
+                    date: "2012-10-16T17:57:28.556094Z"
+                },
+                {
+                    rating: 4,
+                    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                    author: "Paul McVites",
+                    date: "2014-09-05T17:57:28.556094Z"
+                },
+                {
+                    rating: 3,
+                    comment: "Eat it, just eat it!",
+                    author: "Michael Jaikishan",
+                    date: "2015-02-13T17:57:28.556094Z"
+                },
+                {
+                    rating: 4,
+                    comment: "Ultimate, Reaching for the stars!",
+                    author: "Ringo Starry",
+                    date: "2013-12-02T17:57:28.556094Z"
+                },
+                {
+                    rating: 2,
+                    comment: "It's your birthday, we're gonna party!",
+                    author: "25 Cent",
+                    date: "2011-12-02T17:57:28.556094Z"
+                }
+
+            ]
+        };
+
+        $scope.dish = dish;
+
+    }])
+
+    .controller('DishCommentController', ['$scope', function ($scope) {
+
+        //Step 1: Create a JavaScript object to hold the comment from the form
+        $scope.dishComment = {
+            author: "", rating: 5, comment: "", date: ""
+        };
+
+        $scope.submitComment = function () {
+
+            $scope.dishComment.date = new Date().toISOString();
+
+            $scope.dish.comments.push($scope.dishComment);
+
+            $scope.dishCommentForm.$setPristine();
+            $scope.dishComment = {author: "", rating: 5, comment: "", date: ""};
+
+        }
+    }])
+
+;
