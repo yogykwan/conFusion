192b124d8d832fdb5186d8e098ae94a34c3f233f
Jing Guan <jingg.cxy@foxmail.com>
2017-05-16 20:22:53 +0800

    Ionic Modals and Forms
    - Define an Ionic modal and activate it using JavaScript
    - Construct forms using Ionic's support for forms

--- conFusion-Ionic/www/js/controllers.js
+++ conFusion-Ionic/www/js/controllers.js
@@ -41,6 +41,38 @@ angular.module('conFusion.controllers', [])
         $scope.closeLogin();
       }, 1000);
     };
+
+
+    $scope.reservation = {};
+
+    // Create the reserve modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/reserve.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.reserveform = modal;
+    });
+
+    // Triggered in the reserve modal to close it
+    $scope.closeReserve = function() {
+      $scope.reserveform.hide();
+    };
+
+    // Open the reserve modal
+    $scope.reserve = function() {
+      $scope.reserveform.show();
+    };
+
+    // Perform the reserve action when the user submits the reserve form
+    $scope.doReserve = function() {
+      console.log('Doing reservation', $scope.reservation);
+
+      // Simulate a reservation delay. Remove this and replace with your reservation
+      // code if using a server system
+      $timeout(function() {
+        $scope.closeReserve();
+      }, 1000);
+    };
+
   })
 
   .controller('MenuController', ['$scope', 'menuFactory', 'baseURL', function ($scope, menuFactory, baseURL) {
