c79a93446f7e2ff679ac91278598aa1c5104b564
Jing Guan <jingg.cxy@foxmail.com>
2017-05-17 19:36:28 +0800

    Using Local Storage
    - Make use of HTML5 local storage to store and retrieve data within
    your device
    - Design an Angular service that enables you to use the local storage
    within your application

--- conFusion-Ionic/www/js/controllers.js
+++ conFusion-Ionic/www/js/controllers.js
@@ -2,7 +2,7 @@
 
 angular.module('conFusion.controllers', [])
 
-  .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+  .controller('AppCtrl', function ($scope, $ionicModal, $timeout, $localStorage) {
 
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
@@ -12,7 +12,7 @@ angular.module('conFusion.controllers', [])
     //});
 
     // Form data for the login modal
-    $scope.loginData = {};
+    $scope.loginData = $localStorage.getObject('userinfo', '{}');
 
     // Create the login modal that we will use later
     $ionicModal.fromTemplateUrl('templates/login.html', {
@@ -34,6 +34,8 @@ angular.module('conFusion.controllers', [])
     // Perform the login action when the user submits the login form
     $scope.doLogin = function () {
       console.log('Doing login', $scope.loginData);
+
+      $localStorage.storeObject('userinfo', $scope.loginData);
 
       // Simulate a login delay. Remove this and replace with your login
       // code if using a login system
--- conFusion-Ionic/www/js/services.js
+++ conFusion-Ionic/www/js/services.js
@@ -4,7 +4,7 @@ angular.module('conFusion.services', ['ngResource'])
   .constant("baseURL", "http://localhost:3000/")
 
   .factory('menuFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
-      return $resource(baseURL + "dishes/:id", null, {'update': {method: 'PUT'}});
+    return $resource(baseURL + "dishes/:id", null, {'update': {method: 'PUT'}});
   }])
 
   .factory('promotionFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
@@ -45,6 +45,23 @@ angular.module('conFusion.services', ['ngResource'])
     };
 
     return favFac;
+  }])
+
+  .factory('$localStorage', ['$window', function ($window) {
+    return {
+      store: function (key, value) {
+        $window.localStorage[key] = value;
+      },
+      get: function (key, defaultValue) {
+        return $window.localStorage[key] || defaultValue;
+      },
+      storeObject: function (key, value) {
+        $window.localStorage[key] = JSON.stringify(value);
+      },
+      getObject: function (key, defaultValue) {
+        return JSON.parse($window.localStorage[key] || defaultValue);
+      }
+    }
   }])
 
 ;
