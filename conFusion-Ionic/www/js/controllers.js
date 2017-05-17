176dd0fc2a0fb0230c916fa9c94363548448aa3a
Jing Guan <jingg.cxy@foxmail.com>
2017-05-17 12:01:07 +0800

    Popups, Popovers, ActionSheets, Loading and Gestures
    - Make use of Popups to show messages to the users
    - Add popovers and employ action sheets within an application
    - Support user gestures within your Ionic application.

--- conFusion-Ionic/www/js/controllers.js
+++ conFusion-Ionic/www/js/controllers.js
@@ -233,19 +233,28 @@ angular.module('conFusion.controllers', [])
     $scope.leaders = corporateFactory.query();
   }])
 
-  .controller('FavoritesController', ['$scope', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicListDelegate', function ($scope, menuFactory, favoriteFactory, baseURL, $ionicListDelegate) {
-
+  .controller('FavoritesController', ['$scope', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicListDelegate', '$ionicPopup', '$ionicLoading', '$timeout', function ($scope, menuFactory, favoriteFactory, baseURL, $ionicListDelegate, $ionicPopup, $ionicLoading, $timeout) {
     $scope.baseURL = baseURL;
     $scope.shouldShowDelete = false;
+
+    $ionicLoading.show({
+      template: '<ion-spinner></ion-spinner> Loading...'
+    });
 
     $scope.favorites = favoriteFactory.getFavorites();
 
     $scope.dishes = menuFactory.getDishes().query(
       function (response) {
         $scope.dishes = response;
+        $timeout(function () {
+          $ionicLoading.hide();
+        }, 1000);
       },
       function (response) {
         $scope.message = "Error: " + response.status + " " + response.statusText;
+        $timeout(function () {
+          $ionicLoading.hide();
+        }, 1000);
       });
 
     $scope.toggleDelete = function () {
@@ -253,7 +262,19 @@ angular.module('conFusion.controllers', [])
     };
 
     $scope.deleteFavorite = function (index) {
-      favoriteFactory.deleteFromFavorites(index);
+      var confirmPopup = $ionicPopup.confirm({
+        title: 'Confirm Delete',
+        template: 'Are you sure you want to delete this item?'
+      });
+
+      confirmPopup.then(function (res) {
+        if (res) {
+          console.log('Ok to delete');
+          favoriteFactory.deleteFromFavorites(index);
+        } else {
+          console.log('Canceled delete');
+        }
+      });
       $scope.shouldShowDelete = false;
     };
 
