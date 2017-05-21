e052581149f21b2d06f7f715b157a2bfe48e3a41
Jing Guan <jingg.cxy@foxmail.com>
2017-05-21 19:34:33 +0800

    Mongoose ODM
    - Define and construct Mongoose schemas and models
    - Interact with MongoDB server using Mongoose from your Node
    application.

---
+++ conFusion-Express/node-mongoose/models/dishes.js
@@ -0,0 +1,45 @@
+// grab the things we need
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var commentSchema = new Schema({
+    rating:  {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment:  {
+        type: String,
+        required: true
+    },
+    author:  {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+// create a schema
+var dishSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    comments:[commentSchema]
+}, {
+    timestamps: true
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var Dishes = mongoose.model('Dish', dishSchema);
+
+// make this available to our Node applications
+module.exports = Dishes;
---
+++ conFusion-Express/node-mongoose/server.js
@@ -0,0 +1,64 @@
+var mongoose = require('mongoose'),
+    assert = require('assert');
+
+var Dishes = require('./models/dishes');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/conFusion';
+mongoose.connect(url);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    // we're connected!
+    console.log("Connected correctly to server");
+
+    // create a new dish
+    Dishes.create({
+        name: 'Uthapizza',
+        description: 'Test',
+        comments: [
+            {
+                rating: 3,
+                comment: 'This is insane',
+                author: 'Matt Daemon'
+            }
+        ]
+    }, function (err, dish) {
+        if (err) throw err;
+        console.log('Dish created!');
+        console.log(dish);
+
+        var id = dish._id;
+
+        // get all the dishes
+        setTimeout(function () {
+            Dishes.findByIdAndUpdate(id, {
+                $set: {
+                    description: 'Updated Test'
+                }
+            }, {
+                new: true
+            })
+                .exec(function (err, dish) {
+                    if (err) throw err;
+                    console.log('Updated Dish!');
+                    console.log(dish);
+
+                    dish.comments.push({
+                        rating: 5,
+                        comment: 'I\'m getting a sinking feeling!',
+                        author: 'Leonardo di Carpaccio'
+                    });
+
+                    dish.save(function (err, dish) {
+                        console.log('Updated Comments!');
+                        console.log(dish);
+
+                        db.collection('dishes').drop(function () {
+                            db.close();
+                        });
+                    });
+                });
+        }, 3000);
+    });
+});
