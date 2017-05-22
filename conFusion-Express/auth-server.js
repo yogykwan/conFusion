3bc0577b6ca35ffa260f3c1145a249297fbba428
Jing Guan <jingg.cxy@foxmail.com>
2017-05-23 00:00:12 +0800

    Basic Authentication
    - Understood the basics of user authentication
    - Used basic authentication support to authenticate users.

---
+++ conFusion-Express/auth-server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var morgan = require('morgan');
+
+var hostname = 'localhost';
+var port = 3000;
+
+var app = express();
+
+app.use(morgan('dev'));
+
+function auth(req, res, next) {
+    console.log(req.headers);
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        var err = new Error('You are not authenticated!');
+        err.status = 401;
+        next(err);
+        return;
+    }
+
+    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if (user === 'admin' && pass === 'passwd') {
+        next(); // authorized
+    } else {
+        var err = new Error('You are not authenticated!');
+        err.status = 401;
+        next(err);
+    }
+}
+
+app.use(auth);
+
+app.use(express.static(__dirname + '/public'));
+app.use(function (err, req, res, next) {
+    res.writeHead(err.status || 500, {
+        'WWW-Authenticate': 'Basic',
+        'Content-Type': 'text/plain'
+    });
+    res.end(err.message);
+});
+
+app.listen(port, hostname, function () {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
