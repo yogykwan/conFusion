6e166d2ac57dd6fcc0ae59af88c44c7e1f00fd3c
Jing Guan <jingg.cxy@foxmail.com>
2017-05-25 12:55:49 +0800

    Express Sessions
    - Set up the Express server to use express-session to track
    authenticated users.

--- conFusion-Express/auth-server.js
+++ conFusion-Express/auth-server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
-var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var hostname = 'localhost';
 var port = 3000;
@@ -9,11 +10,17 @@ var app = express();
 
 app.use(morgan('dev'));
 
-app.use(cookieParser('12345-67890-09876-54321')); // secret key
+app.use(session({
+    name: 'session-id',
+    secret: '12345-67890-09876-54321',
+    saveUninitialized: true,
+    resave: true,
+    store: new FileStore()
+}));
 
 function auth(req, res, next) {
     console.log(req.headers);
-    if (!req.signedCookies.user) {
+    if (!req.session.user) {
         var authHeader = req.headers.authorization;
         if (!authHeader) {
             var err = new Error('You are not authenticated!');
@@ -26,7 +33,7 @@ function auth(req, res, next) {
         var user = auth[0];
         var pass = auth[1];
         if (user === 'admin' && pass === 'passwd') {
-            res.cookie('user', 'admin', {signed: true});
+            req.session.user = 'admin';
             next(); // authorized
         } else {
             var err = new Error('You are not authenticated!');
@@ -34,7 +41,8 @@ function auth(req, res, next) {
             next(err);
         }
     } else {
-        if (req.signedCookies.user === 'admin') {
+        if (req.session.user === 'admin') {
+            console.log('req.session: ',req.session);
             next();
         }
         else {
