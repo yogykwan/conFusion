16557553a4dccbc92ead478fcbf9983ce89b58a3
Jing Guan <jingg.cxy@foxmail.com>
2017-05-25 12:51:32 +0800

    Cookies
    - Set up your Express application to use cookies.

--- conFusion-Express/auth-server.js
+++ conFusion-Express/auth-server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
 
 var hostname = 'localhost';
 var port = 3000;
@@ -8,31 +9,46 @@ var app = express();
 
 app.use(morgan('dev'));
 
+app.use(cookieParser('12345-67890-09876-54321')); // secret key
+
 function auth(req, res, next) {
     console.log(req.headers);
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-        var err = new Error('You are not authenticated!');
-        err.status = 401;
-        next(err);
-        return;
-    }
+    if (!req.signedCookies.user) {
+        var authHeader = req.headers.authorization;
+        if (!authHeader) {
+            var err = new Error('You are not authenticated!');
+            err.status = 401;
+            next(err);
+            return;
+        }
 
-    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
-    if (user === 'admin' && pass === 'passwd') {
-        next(); // authorized
+        var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+        var user = auth[0];
+        var pass = auth[1];
+        if (user === 'admin' && pass === 'passwd') {
+            res.cookie('user', 'admin', {signed: true});
+            next(); // authorized
+        } else {
+            var err = new Error('You are not authenticated!');
+            err.status = 401;
+            next(err);
+        }
     } else {
-        var err = new Error('You are not authenticated!');
-        err.status = 401;
-        next(err);
+        if (req.signedCookies.user === 'admin') {
+            next();
+        }
+        else {
+            var err = new Error('You are not authenticated!');
+            err.status = 401;
+            next(err);
+        }
     }
 }
 
 app.use(auth);
 
 app.use(express.static(__dirname + '/public'));
+
 app.use(function (err, req, res, next) {
     res.writeHead(err.status || 500, {
         'WWW-Authenticate': 'Basic',
